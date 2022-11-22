a1e14459630f4a55f0a1588646e76ffb78592709
yongbin <rhdydqls1024@naver.com>
2022-11-22 16:14:00 +0900

    :sparkles: :: discovery-service

---
+++ discovery-service/src/test/java/com/bamboo/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamboo.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
