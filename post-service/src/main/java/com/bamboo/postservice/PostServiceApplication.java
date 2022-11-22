36cf6bdf2524d220233e2e69326cb9df8c2bab91
yongbin <rhdydqls1024@naver.com>
2022-11-22 16:34:16 +0900

    :sparkles: :: post-service

---
+++ post-service/src/main/java/com/bamboo/postservice/PostServiceApplication.java
@@ -0,0 +1,15 @@
+package com.bamboo.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
