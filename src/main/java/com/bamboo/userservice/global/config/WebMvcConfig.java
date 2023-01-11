d2ccab09dec51c98c614ddd793293308fee3978e
jiseok419 <pyosik0419@gmail.com>
2023-01-11 22:46:02 +0900

    :sparkles: :: CORS/Intercepter 추가

---
+++ src/main/java/com/bamboo/userservice/global/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.bamboo.userservice.global.config;
+
+import com.bamboo.userservice.global.interceptor.MyInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final MyInterceptor myInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(myInterceptor);
+    }
+}
