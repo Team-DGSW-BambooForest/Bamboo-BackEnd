71b159f485d78de8956cec9e13f12f4ad11b78d8
jiseok419 <pyosik0419@gmail.com>
2023-01-11 22:48:21 +0900

    :sparkles: :: UserService 추가

---
+++ src/main/java/com/bamboo/userservice/domain/user/service/UserService.java
@@ -0,0 +1,30 @@
+package com.bamboo.userservice.domain.user.service;
+
+import com.bamboo.userservice.domain.auth.presentation.dto.api.DOpenApiDto;
+import com.bamboo.userservice.domain.auth.presentation.dto.api.DodamInfoDto;
+import com.bamboo.userservice.domain.user.UserEntity;
+import com.bamboo.userservice.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    @Transactional
+    public UserEntity save(DOpenApiDto dOpenApiDto) {
+        DodamInfoDto data = dOpenApiDto.getDodamInfoData();
+        UserEntity user = DodamInfoDto.toEntity(data);
+        if(isExisted(user)){
+            throw new UserEntity.AlreadyExistedException();
+        }
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    protected boolean isExisted(UserEntity user) {
+        return userRepository.existsByGradeAndNumberAndRoom(user.getGrade(), user.getNumber(), user.getRoom());
+    }
+}
