e8b668005dba379576f93dc7c4f06fdfb75227b0
pea06 <101560095+pea06@users.noreply.github.com>
2023-01-12 16:04:34 +0900

    ✨ : repository 추가

--- post-service/src/main/java/com/bamboo/postservice/domain/repository/HashTagRepository.java
+++ post-service/src/main/java/com/bamboo/postservice/domain/repository/HashTagRepository.java
@@ -1,9 +1,18 @@
 package com.bamboo.postservice.domain.repository;
 
+
 import com.bamboo.postservice.domain.HashTag;
+import com.bamboo.postservice.presentation.dto.reponse.TagRo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+    List<TagRo> findByPost_PostId(Long id);
+
+
+    List<HashTag> findByHashTagContaining (String hashtag);
 }
--- post-service/src/main/java/com/bamboo/postservice/domain/repository/PostRepository.java
+++ post-service/src/main/java/com/bamboo/postservice/domain/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.bamboo.postservice.domain.repository;
 
 import com.bamboo.postservice.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContaining(String title, Pageable pageable);
+
+    Post findByPostId(long id);
+
 }
