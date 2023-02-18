b221b29908d8c4592148f3ef5a0513192c92eb6f
jiseok419 <pyosik0419@gmail.com>
2023-02-18 17:56:38 +0900

    :zap: :: user-service spring cloud 설정

--- user-service/src/main/java/com/bamboo/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/bamboo/userservice/UserServiceApplication.java
@@ -2,10 +2,10 @@ package com.bamboo.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
