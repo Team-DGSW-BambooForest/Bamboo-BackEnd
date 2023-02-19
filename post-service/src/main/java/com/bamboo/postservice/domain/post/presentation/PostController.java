a5f895af77b8ad9cfd4a6f4b07853a91a2192844
yongbin <rhdydqls1024@naver.com>
2023-02-19 20:58:15 +0900

    스웨거 오류 수정

--- post-service/src/main/java/com/bamboo/postservice/domain/post/presentation/PostController.java
+++ post-service/src/main/java/com/bamboo/postservice/domain/post/presentation/PostController.java
@@ -5,7 +5,6 @@ import com.bamboo.postservice.global.annotation.AuthToken;
 import com.bamboo.postservice.domain.post.presentation.dto.reponse.PostRo;
 import com.bamboo.postservice.domain.post.presentation.dto.request.PostRequest;
 import com.bamboo.postservice.domain.post.service.PostService;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
     private final PostService postService;
     @AuthToken
-    @ApiOperation(value = "게시글 등록")
     @PostMapping("/create")
     public ResponseEntity<?> creatPost(@RequestBody @Validated PostRequest request,
                                        @RequestAttribute String author)
@@ -26,33 +24,28 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "단일 게시글 조회")
     public PostRo getPostById(@PathVariable("id") Long id) {
         return postService.getPostById(id);
     }
 
     @GetMapping("/list")
-    @ApiOperation(value = "전체 게시글 조회")
     public PostListRo getALlPost(@RequestParam(value = "page", defaultValue = "1") Integer page) {
         return postService.getAllPost(page);
     }
 
     @GetMapping("/title/{title}")
-    @ApiOperation(value = "제목으로 게시글 조회")
     public PostListRo getPostByTitle(@RequestParam(value = "page", defaultValue = "1") Integer page,
                                      @PathVariable("title") String title) {
         return postService.getPostByTitle(page, title);
     }
 
     @GetMapping("/tag/{tag}")
-    @ApiOperation(value = "테그로 게시글 조회")
     public PostListRo getPostByHashtag(@RequestParam(value = "page", defaultValue = "1") Integer page,
                                        @PathVariable("tag") String tag) {
          return postService.getPostByHashTag(page, tag);
     }
 
     @GetMapping("/list/hold")
-    @ApiOperation(value = "허용 되지 않은 게시글 조회")
     public PostListRo getUnauthorizedPost(@RequestParam(value = "page", defaultValue = "1") Integer page) {
         return postService.getUnauthorizedPost(page);
     }
--- post-service/src/main/java/com/bamboo/postservice/global/config/SwaggerConfig.java
+++ post-service/src/main/java/com/bamboo/postservice/global/config/SwaggerConfig.java
@@ -1,34 +1,24 @@
 package com.bamboo.postservice.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket apiV1() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bamboo.postservice"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    public OpenAPI openAPI() {
+        Info info = new Info()
                 .title("대대숲 post-service api")
-                .description("대대숲 post-service api 문서, ip : 3.36.150.248")
-                .version("Version 1.0")
-                .build();
+                .version("version 1.0")
+                .description("대대숲 post-service api 문서, ip : 3.36.150.248");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
 
 }
