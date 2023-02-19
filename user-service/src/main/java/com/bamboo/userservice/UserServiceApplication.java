2d5cdb28615b4760819f1a70a161bcc3924781fc
yongbin <rhdydqls1024@naver.com>
2023-02-19 13:41:33 +0900

    Access Token 만료 오류 해결

--- user-service/src/main/java/com/bamboo/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/bamboo/userservice/UserServiceApplication.java
@@ -1,11 +1,15 @@
 package com.bamboo.userservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ConfigurationPropertiesScan
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/bamboo/userservice/global/config/AppProperties.java
+++ user-service/src/main/java/com/bamboo/userservice/global/config/AppProperties.java
@@ -1,12 +1,16 @@
 package com.bamboo.userservice.global.config;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@Data
-@Configuration
+@Getter
+@ConstructorBinding
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "app")
+@RefreshScope
 public class AppProperties {
     private String secret;
     private String refreshSecret;
