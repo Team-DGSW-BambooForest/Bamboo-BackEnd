1af426ed7377be607d9c676104745fa73e6826b9
yongbin <rhdydqls1024@naver.com>
2023-02-21 20:52:54 +0900

    cors 문제 해결해봄

---
+++ apigateway-service/src/main/java/com/bamboo/apigatewayservice/config/CORSConfiguration.java
@@ -0,0 +1,18 @@
+package com.bamboo.apigatewayservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
+    }
+
+}
