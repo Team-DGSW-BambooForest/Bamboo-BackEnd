e89d8746f060cd437c5e2f6f5a58375543c2a15d
강지석 <pyosik0419@gmail.com>
2023-02-21 14:26:27 +0900

    :memo: :: cors 수정

--- user-service/src/main/java/com/bamboo/userservice/global/config/WebMvcConfig.java
+++ user-service/src/main/java/com/bamboo/userservice/global/config/WebMvcConfig.java
@@ -15,8 +15,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
     }
 
     @Override
