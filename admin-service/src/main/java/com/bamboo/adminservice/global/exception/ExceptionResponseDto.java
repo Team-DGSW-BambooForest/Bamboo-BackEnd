5848c8a17d7df783ed8dafdb7d7e7c81cbc9a845
jiseok419 <pyosik0419@gmail.com>
2023-02-23 13:25:15 +0900

    :zap: :: 최상위 예외처리 파일 추가

---
+++ admin-service/src/main/java/com/bamboo/adminservice/global/exception/ExceptionResponseDto.java
@@ -0,0 +1,20 @@
+package com.bamboo.adminservice.global.exception;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ExceptionResponseDto {
+    @JsonProperty(value = "timestamp", index = 1)
+    private final LocalDateTime timeStamp = LocalDateTime.now();
+    private final int status;
+    private final HttpStatus error;
+    private final String message;
+}
