52a3edcb8d6f4f9b4f5f50c610958ecb559db0c4
jiseok419 <pyosik0419@gmail.com>
2023-02-23 15:19:12 +0900

    :memo: :: cors 삭제

--- admin-service/src/main/java/com/bamboo/adminservice/global/config/WebMvcConfig.java
+++ admin-service/src/main/java/com/bamboo/adminservice/global/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package com.bamboo.adminservice.global.config;
 import com.bamboo.adminservice.global.intercetor.HttpIntercetor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,14 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final HttpIntercetor httpIntercetor;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowCredentials(false);
-    }
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(httpIntercetor);
