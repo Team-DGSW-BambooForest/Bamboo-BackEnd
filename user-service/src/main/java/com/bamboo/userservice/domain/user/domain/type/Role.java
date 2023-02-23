7d8e546eab3b9ba28ac9bdae5a4c026073441792
jiseok419 <pyosik0419@gmail.com>
2023-02-23 15:04:32 +0900

    :recycle: :: User Service Role 수정

--- user-service/src/main/java/com/bamboo/userservice/domain/user/domain/type/Role.java
+++ user-service/src/main/java/com/bamboo/userservice/domain/user/domain/type/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_ADMIN", "어드민"),
+    ADMIN("ROLE_ADMIN", "어드민"),
     USER("ROLE_USER", "일반 사용자");
 
     private final String key;
