eef52b1c0d4e21224ce1d86c43d13ad4791e2d2c
jiseok419 <pyosik0419@gmail.com>
2023-02-23 15:19:42 +0900

    :memo: :: swagger 작성

---
+++ admin-service/src/main/java/com/bamboo/adminservice/global/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.bamboo.adminservice.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("대대숲 admin-service api")
+                .version("version 1.0")
+                .description("대대숲 admin-service api 문서, ip : 3.36.150.248");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
