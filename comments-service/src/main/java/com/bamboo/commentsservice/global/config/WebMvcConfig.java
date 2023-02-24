4688bc809d0dea16417e4b64696c0c2b255210f7
pea06 <101560095+pea06@users.noreply.github.com>
2023-02-24 21:47:24 +0900

    오류 수정

--- comments-service/src/main/java/com/bamboo/commentsservice/global/config/WebMvcConfig.java
+++ comments-service/src/main/java/com/bamboo/commentsservice/global/config/WebMvcConfig.java
@@ -2,10 +2,12 @@ package com.bamboo.commentsservice.global.config;
 
 import com.bamboo.commentsservice.global.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
