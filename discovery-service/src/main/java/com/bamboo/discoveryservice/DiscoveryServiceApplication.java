86e8519a1fa55d9ece84d819f35e70278f821ede
yongbin <rhdydqls1024@naver.com>
2023-03-07 21:17:16 +0900

    :memo: :: jenkins CI/CD Test

--- discovery-service/src/main/java/com/bamboo/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/bamboo/discoveryservice/DiscoveryServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
