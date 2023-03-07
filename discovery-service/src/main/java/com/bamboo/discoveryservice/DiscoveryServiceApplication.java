30820d17b5aed980d2e473dfc6fdd595478e8d9f
yongbin <rhdydqls1024@naver.com>
2023-03-07 22:09:03 +0900

    :memo: :: Jenkins CI/CD Github WebHooks Test

--- discovery-service/src/main/java/com/bamboo/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/bamboo/discoveryservice/DiscoveryServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
